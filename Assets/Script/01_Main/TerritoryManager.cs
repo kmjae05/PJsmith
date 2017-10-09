0e9a77e99ca9b6308bbfc6d186b1e1cdb243026b
Jae <kjhee940@naver.com>
2017-10-09 22:13:27 +0900

    건설 진행 팝업 띄ㅟㄱ

--- Assets/Script/01_Main/TerritoryManager.cs
+++ Assets/Script/01_Main/TerritoryManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,8 +9,14 @@ public class TerritoryManager : MonoBehaviour
 
     private GameObject uiPanel;
     private GameObject buildInfoPopup;
+    private GameObject BeUnderPopup;
+
     private GameObject BlackBack;
     private Button CancleButton;
+    private GameObject BottomMenuLock;
+
+    private GameObject SystemPopup;
+    private Button sys_yesButton;
 
 
     private List<GameObject> mineObj;           //광산 스팟
@@ -25,8 +32,13 @@ public class TerritoryManager : MonoBehaviour
     {
         uiPanel = GameObject.Find("Menu").transform.Find("TerritoryPopup/UIPanel").gameObject;
         buildInfoPopup = GameObject.Find("System").transform.Find("BuildInfoPopup").gameObject;
+        BeUnderPopup = GameObject.Find("System").transform.Find("BeUnderPopup").gameObject;
         BlackBack = uiPanel.transform.Find("BlackBack").gameObject;
         CancleButton = BlackBack.transform.Find("CancleButton").gameObject.GetComponent<Button>();
+        BottomMenuLock = uiPanel.transform.Find("BottomMenu/BottomMenuLock").gameObject;
+        SystemPopup = GameObject.Find("System").transform.Find("SystemPopup").gameObject;
+        sys_yesButton = SystemPopup.transform.Find("UIPanel/YesButton").gameObject.GetComponent<Button>();
+
 
         mineObj = new List<GameObject>();
         bottonButtonList = new List<GameObject>();
@@ -57,8 +69,24 @@ public class TerritoryManager : MonoBehaviour
             //건설 중
             if (MineData.instance.getMineList()[i].buildState == "beunder")
             {
-                //시간 빼기
                 //시간 0이면 건설완료 상태로
+                if (MineData.instance.getMineList()[i].buildTime < 0)
+                {
+                    MineData.instance.getMineList()[i].buildState = "complete";
+                    //MineData.instance.getMineList()[i].miningState = true;
+                    mineObj[i].transform.Find("Text").gameObject.GetComponent<Text>().text = "건설 완료";
+                    BottomMenuLock.SetActive(false);
+                    if (BeUnderPopup.activeInHierarchy) BeUnderPopup.SetActive(false);
+
+                    mineObj[i].GetComponent<Button>().onClick.RemoveAllListeners();
+                }
+                //시간 빼기
+                else
+                {
+                    MineData.instance.getMineList()[i].buildTime -= Time.deltaTime;
+                    mineObj[i].transform.Find("Text").gameObject.GetComponent<Text>().text = ((int)MineData.instance.getMineList()[i].buildTime).ToString();
+                    BeUnderPopup.transform.Find("UIPanel/InfoBox/TimeText").gameObject.GetComponent<Text>().text = ((int)MineData.instance.getMineList()[i].buildTime).ToString();
+                }
 
             }
             //건설 완료 & 채굴 상태
@@ -165,7 +193,7 @@ public class TerritoryManager : MonoBehaviour
         }
 
         obj.GetComponent<Button>().onClick.RemoveAllListeners();
-        obj.GetComponent<Button>().onClick.AddListener(() => BuildCondition(obj));
+        obj.GetComponent<Button>().onClick.AddListener(() => BuildCondition(obj, num));
 
         Color clr = obj.transform.Find("Image").gameObject.GetComponent<Image>().color;
         obj.transform.Find("Image").gameObject.GetComponent<Image>().color = new Color(clr.r, clr.g, clr.b, 0.5f);
@@ -173,7 +201,10 @@ public class TerritoryManager : MonoBehaviour
         obj.transform.Find("Text").gameObject.SetActive(true);
         obj.transform.Find("DottedCircle").gameObject.SetActive(false);
         obj.transform.Find("pickax").gameObject.SetActive(false);
+        //광산에 정보 저장
         MineData.instance.getMineList()[num].buildState = "beunder";
+        MineData.instance.getMineList()[num].type = info.type;
+        MineData.instance.getMineList()[num].buildTime = info.buildTime;
 
         //나머지 nothing없애고 건설선택 블랙백 없애기
         for (int j = 0; j < MineData.instance.getMineList().Count; j++)
@@ -187,19 +218,41 @@ public class TerritoryManager : MonoBehaviour
             }
         }
         BlackBack.SetActive(false);
-
-
+        //하단메뉴 잠금
+        BottomMenuLock.SetActive(true);
 
     }
 
     //건설 진행 중 버튼 (현황 창
-    public void BuildCondition(GameObject obj)
+    public void BuildCondition(GameObject obj, int num)
     {
-        Debug.Log(obj.transform.name);
-
+        string type = MineData.instance.getMineList()[num].type;
+        BeUnderPopup.transform.Find("UIPanel/BackBox/TitleText").gameObject.GetComponent<Text>().text = type + " 광산 건설 중";
         //팝업 올리기
+        BeUnderPopup.SetActive(true);
 
-        //건설 취소 버튼
+        //즉시 완료 버튼
+        BeUnderPopup.transform.Find("UIPanel/CancleButton").gameObject.GetComponent<Button>().onClick.RemoveAllListeners();
+        BeUnderPopup.transform.Find("UIPanel/CancleButton").gameObject.GetComponent<Button>().onClick.AddListener(() => {
+            SystemPopup.SetActive(true);
+
+            SystemPopup.transform.Find("UIPanel/BackBox/TitleText").GetComponent<Text>().text = "즉시 완료";
+            SystemPopup.transform.Find("UIPanel/InfoText").GetComponent<Text>().text = "보석 50개를 사용하여 즉시 완료하시겠습니까?";
+            sys_yesButton.GetComponent<Button>().onClick.RemoveAllListeners();      //버튼 리스너 모두 삭제
+            sys_yesButton.GetComponent<Button>().onClick.AddListener(() => {
+
+            });
+
+        });
+
+
+        //건설 취소 버튼->systempopup
+        BeUnderPopup.transform.Find("UIPanel/CancleButton").gameObject.GetComponent<Button>().onClick.RemoveAllListeners();
+        BeUnderPopup.transform.Find("UIPanel/CancleButton").gameObject.GetComponent<Button>().onClick.AddListener(() => {
+            SystemPopup.SetActive(true);
+
+
+        });
 
 
     }
